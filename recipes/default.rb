8d73ff1c2aa2264986e1e2498a9d60d79c98ea4d
Darren Birkett <darren.birkett@gmail.com>
2012-11-16 15:33:58 +0000

    tidy comments out of recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -60,30 +60,6 @@ service "haproxy" do
   action [ :enable, :start ]
 end
 
-#options = {}
-#node["openstack"]["services"].each do |svc|
-#  name = "#{svc.namespace}-#{svc.service}"
-#  options[name] = {}
-#  endpoint = get_access_endpoint(svc.role, svc.namespace, svc.service)
-#  options[name]["listen_port"] = endpoint["port"]
-#  server_list = get_realserver_endpoints(svc.role, svc.namespace, svc.service)
-#  server_list.each do |server|
-#    # Collect each server into the array
-#    tmp_hash = {"host" => server["host"], "port" => server["port"]}
-#    options[name]["servers"] << tmp_hash
-#  end
-#end
-
-#options = {
-#  "nova-api" => {
-#    "listen_port" => "8774",
-#    "servers" => {
-#      {"name" => "foo1", "host" => "x.x.x.x", "port" => "8774"},
-#      {"name" => "foo2", "host" => "y.y.y.y", "port" => "8774"}
-#    }
-#  }
-#}
-
 template "/etc/haproxy/haproxy.cfg" do
   source "haproxy.cfg.erb"
   owner "root"
@@ -94,23 +70,6 @@ template "/etc/haproxy/haproxy.cfg" do
   )
   notifies :restart, resources(:service => "haproxy"), :immediately
 end
-
-
-# iterate through the services in the attributes file
-# and create a config with all the discovered listening servers
-#node['openstack']['services'].each do |svc|
-#  servers = {}
-#  name = "#{svc['namespace']}-#{svc['service']}"
-#  endpoint = get_access_endpoint(svc['role'], svc['namespace'], svc['service'])
-#  listen_port = endpoint['port']
-#  server_list = get_realserver_endpoints(svc['role'], svc['namespace'], svc['service'])
-#  backend = 1
-#  server_list.each do |server|
-#    # push each server into the has
-#    servers["#{name}-#{backend}"] = {"host" => server["host"], "port" => server["port"]}
-#    backend += 1
-#  end
-
 
 node['openstack']['services'].each_key do |name|
   oshaproxy_configalt "#{name}" do
